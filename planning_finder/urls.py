744d16ebf05a32a98d361b2d4a99380c042e5315
Brian Lynch (Digital) <brian.lynch@wipro.com>
2020-04-06 11:40:10 +0100

    create basic home page

--- planning_finder/urls.py
+++ planning_finder/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home.views import get_home
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', get_home, name="home"),
 ]
