13b3c9ceece1e2205444f4bf2bfbebe3f02e2de3
Brian Lynch (Digital) <brian.lynch@wipro.com>
2020-04-06 18:27:42 +0100

    add register form

--- planning_finder/urls.py
+++ planning_finder/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from home.views import get_home
+from users import views as user_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', get_home, name="home"),
+    path('register/', user_views.register, name="register"),
 ]
