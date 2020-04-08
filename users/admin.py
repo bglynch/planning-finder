81180a5146de06c8b2959c8e3bedeb3fd71aa7f6
Brian Lynch (Digital) <brian.lynch@wipro.com>
2020-04-08 09:53:10 +0100

    created Profile model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
+from .models import Profile
+
 
 # Register your models here.
+@admin.register(Profile)
+class ShopAdmin(OSMGeoAdmin):
+    list_display = ('user', 'location')
--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.gis.db import models
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.PointField()
