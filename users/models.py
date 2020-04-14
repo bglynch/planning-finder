4d637dd728f9851d01b4060887711dfc4d523251
Brian Lynch (Digital) <brian.lynch@wipro.com>
2020-04-14 12:07:52 +0100

    auto create profile when user registers

--- users/models.py
+++ users/models.py
@@ -1,8 +1,20 @@
 from django.contrib.auth.models import User
 from django.contrib.gis.db import models
+from django.contrib.gis.geos import Point
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     location = models.PointField()
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance, location=Point(x=-6, y=53))
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- users/views.py
+++ users/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, ProfileForm
 
 
 # Create your views here.
 
 def register(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
+        user_form = UserRegisterForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect('home')
     else:
-        form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
-
+        user_form = UserRegisterForm()
+    return render(request, 'users/register.html', {'user_form': user_form})
 
 @login_required
 def profile(request):
