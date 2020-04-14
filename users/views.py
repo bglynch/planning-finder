cd036de4e9547b8768eb2baea29d015704c3f89d
Brian Lynch (Digital) <brian.lynch@wipro.com>
2020-04-14 12:34:09 +0100

    auto login user on registration

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, ProfileForm
 
@@ -10,10 +11,16 @@ def register(request):
         user_form = UserRegisterForm(request.POST)
         if user_form.is_valid():
             user_form.save()
+            new_user = authenticate(
+                username=user_form.cleaned_data['username'],
+                password=user_form.cleaned_data['password1'],
+            )
+            login(request, new_user)
             return redirect('home')
     else:
         user_form = UserRegisterForm()
     return render(request, 'users/register.html', {'user_form': user_form})
+
 
 @login_required
 def profile(request):
