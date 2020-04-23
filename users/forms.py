efe17a0081cc0abaf9ff5d0f6424374251d3ef29
bglynch <bglynch17@gmail.com>
2020-04-23 09:56:05 +0100

    backend/updated user tests

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from leaflet.forms.widgets import LeafletWidget
 from leaflet.forms.fields import PointField
