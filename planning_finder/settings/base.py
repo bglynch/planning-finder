4f707b5de1ee593a5cb98b8c334f23d5168ed1f4
bglynch <bglynch17@gmail.com>
2020-05-25 12:39:00 +0100

    updated settings

--- planning_finder/settings/base.py
+++ planning_finder/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bglynch-planning-finder.herokuapp.com']
 
 # Application definition
 
