e657a87410d199df134fd9b5f2920691efa6d724
bglynch <bglynch17@gmail.com>
2020-05-25 10:59:19 +0100

    removed Pillow, updated prod settings

--- planning_finder/settings/prod.py
+++ planning_finder/settings/prod.py
@@ -9,33 +9,24 @@ DEBUG = True
 Database = re.split(r"(:\/\/|@|:)", config('DATABASE_URL'))
 
 DATABASES = {
-   'default': {
-      'ENGINE': 'django.contrib.gis.db.backends.postgis',
-      'NAME': Database[-1].split("/")[-1],
-      'USER': Database[2],
-      'PASSWORD': Database[4],
-      'HOST': Database[6],
-      'PORT': Database[-1].split("/")[0]
-   }
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': Database[-1].split("/")[-1],
+        'USER': Database[2],
+        'PASSWORD': Database[4],
+        'HOST': Database[6],
+        'PORT': Database[-1].split("/")[0]
+    }
 }
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_S3_HOST = 's3-eu-west-1.amazonaws.com'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-MEDIAFILES_LOCATION = 'media'
-MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
