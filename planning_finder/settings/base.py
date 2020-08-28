e8789d55d13f09af1face276c55562e1af14f41a
bglynch <bglynch17@gmail.com>
2020-08-28 15:29:47 +0100

    extracted ALLOWED_HOSTS to enviornment variables

--- planning_finder/settings/base.py
+++ planning_finder/settings/base.py
@@ -16,13 +16,9 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bglynch-planning-finder.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', config('ALLOWED_HOST', default="")]
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -78,8 +74,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'planning_finder.wsgi.application'
 
 # Password validation
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
@@ -88,8 +82,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Internationalization
-# https://docs.djangoproject.com/en/2.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
@@ -97,13 +89,14 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-LOGIN_REDIRECT_URL = 'home'
+# Redirect options
 LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
+# Custom User Settings
 AUTH_USER_MODEL = 'users.CustomUser'
 ACCOUNT_USER_MODEL_USERNAME_FIELD = None
 ACCOUNT_USERNAME_REQUIRED = False
@@ -116,8 +109,10 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
+# Spatialite
 SITE_ID = 2
 
+# OAuth for Google Login
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
         'SCOPE': [
@@ -130,6 +125,7 @@ SOCIALACCOUNT_PROVIDERS = {
     }
 }
 
+# Google email for password reset
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -137,5 +133,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
+
+# Stripe payments
 STRIPE_PUBLISHABLE = config('STRIPE_PUBLISHABLE_KEY')
 STRIPE_SECRET = config('STRIPE_SECRET_KEY')
