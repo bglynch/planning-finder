28032f69b49cfe630ccd2cad0c1996b5462614d1
bglynch <bglynch17@gmail.com>
2020-09-01 16:14:24 +0100

    frontend/added bookmark icon to list items

--- static/js/functions.js
+++ static/js/functions.js
@@ -76,7 +76,10 @@ function createListItem(geoJSONPoint) {
             <div class="col-12 main-font-big">
                 ${geoJSONPoint.properties['ApplicationNumber']}
                 <svg class="fly-to ml-1" onclick="flyToPlace(${geoJSONPoint.geometry.coordinates[1]}, ${geoJSONPoint.geometry.coordinates[0]})">
-                <path d="M22,16v-2l-8.5-5V3.5C13.5,2.67,12.83,2,12,2s-1.5,0.67-1.5,1.5V9L2,14v2l8.5-2.5V19L8,20.5L8,22l4-1l4,1l0-1.5L13.5,19 v-5.5L22,16z"/>
+                    <path d="M22,16v-2l-8.5-5V3.5C13.5,2.67,12.83,2,12,2s-1.5,0.67-1.5,1.5V9L2,14v2l8.5-2.5V19L8,20.5L8,22l4-1l4,1l0-1.5L13.5,19 v-5.5L22,16z"/>
+                </svg>
+                <svg viewBox="0 0 16 16" class="bi bi-bookmark" xmlns="http://www.w3.org/2000/svg"">
+                    <path  d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"></path>
                 </svg>
             </div>
             <div class="planning-status main-font-normal">${setDecision(geoJSONPoint.properties['Decision'])}
@@ -84,10 +87,8 @@ function createListItem(geoJSONPoint) {
                     <circle cx="10" cy="10" r="4" stroke="${geoJSONPoint.properties['colour']}" stroke-width="1" fill="${geoJSONPoint.properties['colour']}" fill-opacity="0.8" />
                 </svg>
             </div>
-
             <div class="col-12 main-font-normal"><span class="main-font-small" style="padding-right: 7px;">Application Date:</span> ${new Date(geoJSONPoint.properties['ReceivedDate']).toLocaleDateString("en-GB")}</div>
             <div class="col-12 main-font-normal"><span class="main-font-small" style="padding-right: 63px;">Status:</span>${geoJSONPoint.properties['ApplicationStatus']}</div>
-
             <div class="col-12 mt-2 mt-sm-3">
                 <img src="${setCouncilLogo(geoJSONPoint.properties['PlanningAuthority'])}" alt="" class="float-left mr-2 mt-2" height="20" width="20">
                 <div class="float-left main-font-small mt-2">${geoJSONPoint.properties['PlanningAuthority']}</div>
