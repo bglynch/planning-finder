2189d06bf88b4b0fc7f5a811a4f0d6dd1ded7121
bglynch <bglynch17@gmail.com>
2020-09-02 12:49:44 +0100

    update views to add/delete bookmarks

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
+from bookmark.models import Favourite
 
 
 @login_required
@@ -8,13 +9,21 @@ def bookmark_application(request):
     if request.method == 'POST':
         current_user = request.user
         data = request.POST.get('data')
+        bookmark = Favourite()
+        bookmark.planning_id = data
+        bookmark.user = current_user
+
+        bookmark.save()
         print(f"Bookmarking application {data}, for {current_user.id}")
         return HttpResponse(status=200)
+
 
 @login_required
 def remove_bookmark(request):
     if request.method == 'POST':
         current_user = request.user
         data = request.POST.get('data')
+        bookmark = Favourite.objects.filter(user=current_user).filter(planning_id=data)
+        bookmark.delete()
         print(f"Removing bookmark application {data}, for {current_user.id}")
         return HttpResponse(status=200)
