bcec7dc8ce32b5f3c611df4400fe6927b59b28f4
bglynch <bglynch17@gmail.com>
2020-09-03 10:56:54 +0100

    added fucntion to create bookmark on profile page

--- static/js/functions.js
+++ static/js/functions.js
@@ -128,6 +128,39 @@ function createBookmarkedListItem(geoJSONPoint) {
     return divhtml
 }
 
+function createBookmarkedListItemProfilePage(geoJSONPoint) {
+    divhtml = `
+        <div id="${geoJSONPoint.properties['ApplicationNumber'].replace(/\//g, "").trim()}" class="list-group-item">
+            <div class="col-12 main-font-big">
+                ${geoJSONPoint.properties['ApplicationNumber']}
+                <svg class="fly-to ml-1" onclick="flyToPlace(${geoJSONPoint.geometry.coordinates[1]}, ${geoJSONPoint.geometry.coordinates[0]})">
+                    <path d="M22,16v-2l-8.5-5V3.5C13.5,2.67,12.83,2,12,2s-1.5,0.67-1.5,1.5V9L2,14v2l8.5-2.5V19L8,20.5L8,22l4-1l4,1l0-1.5L13.5,19 v-5.5L22,16z"/>
+                </svg>
+                <svg viewBox="0 0 16 16" class="bi bi-bookmark click-active active" xmlns="http://www.w3.org/2000/svg" data-app-number="${geoJSONPoint.properties['ApplicationNumber'].trim()}">
+                    <path  d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5V2z"></path>
+                </svg>
+            </div>
+            <div class="planning-status main-font-normal">${setDecision(geoJSONPoint.properties['Decision'])}
+                <svg height="20" width="20">
+                    <circle cx="10" cy="10" r="4" stroke="${geoJSONPoint.properties['colour']}" stroke-width="1" fill="${geoJSONPoint.properties['colour']}" fill-opacity="0.8" />
+                </svg>
+            </div>
+            <div class="col-12 main-font-normal"><span class="main-font-small" style="padding-right: 7px;">Application Date:</span> ${new Date(geoJSONPoint.properties['ReceivedDate']).toLocaleDateString("en-GB")}</div>
+            <div class="col-12 mt-2 mt-sm-3">
+                <img src="${setCouncilLogo(geoJSONPoint.properties['PlanningAuthority'])}" alt="" class="float-left mr-2 mt-2" height="20" width="20">
+                <div class="float-left main-font-small mt-2">${geoJSONPoint.properties['PlanningAuthority']}</div>
+                <a target="_blank" rel="noopener" class="btn btn-primary float-right main-font-normal" href="${geoJSONPoint.properties['ApplicationUrl']}" style="color: #fbf8f3;font-size: 12px;font-weight: 500;letter-spacing: 1px;">View 
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-link-45deg" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4.715 6.542L3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.001 1.001 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"></path>
+                        <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 0 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 0 0-4.243-4.243L6.586 4.672z"></path>
+                    </svg>
+                </a>
+            </div>
+        </div>
+        `
+    return divhtml
+}
+
 function flyToPlace(lat, lon) {
     map.flyTo([lat, lon], 20, { duration: 1 })
 }
